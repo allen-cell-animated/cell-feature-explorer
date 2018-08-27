ea253533d5888edf1a4f07e8114f4e32133b3e61
Megan Riel-Mehan <meganrm@gmail.com>
2018-08-27 13:42:32 -0700

    Set up app to render thumbnail gallery and and nav menu

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
-import PlotView from "../PlotView";
+import { connect } from "react-redux";
+
+import ColorByMenu from "../../containers/ColorByMenu";
+import { State } from "../../state/types";
+import MainPlotContainer from "../MainPlotContainer";
+import ThumbnailGallery from "../ThumbnailGallery";
 
 const styles = require("./style.css");
 
 export default class App extends React.Component<{}, {}> {
     public render() {
         return (
-            <div className={styles.container}>Hello world
-                <PlotView />
+            <div className={styles.container}>
+                <div className={styles.plotView} >
+                    <ThumbnailGallery />
+                    <MainPlotContainer />
+                </div>
+
+                <div className={styles.colorMenu}>
+                    <ColorByMenu />
+                </div>
+
             </div>
         );
     }
