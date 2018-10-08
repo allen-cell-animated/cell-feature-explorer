82bf3ac01e832c933aca5e41992c24c6da95c1c8
Megan Riel-Mehan <meganrm@gmail.com>
2018-10-08 12:08:50 -0700

    added iframe

---
+++ src/components/CellViewer/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { CELL_VIEWER_URL } from "../../constants/index";
+
+const styles = require("./style.css");
+
+interface CellViewerProps {
+    cellName: string;
+}
+
+const CellViewer: React.SFC<CellViewerProps> = (props) => {
+    // ?legacyName_1_2=AICS-10/AICS-10_5_5
+    const { cellName } = props;
+    if (!cellName) {
+        return null;
+    }
+    const cellLine = cellName.split("_")[0];
+
+    return (
+        <iframe
+            frameborder={0}
+            className={styles.cellViewer}
+            src={`${CELL_VIEWER_URL}?legacyName_1_2=${cellLine}/${cellName}`}
+
+        />
+    );
+};
+
+export default CellViewer;
