89847b0fdb8eee7720c67b88ccf3f138d61fb9c0
Dan Toloudis <danielt@alleninstitute.org>
2018-10-22 17:21:44 -0700

    put data files in data/ subdir, and ensure that gradle bundle, start, and test tasks are working by making sure all three can access the defined server urls

---
+++ scripts/mock-server-urls.js
@@ -0,0 +1,5 @@
+const DevServers = require('../webpack/server-urls-dev.json')
+
+global.CELL_VIEWER_URL = DevServers.CELL_VIEWER_URL;
+global.BASE_API_URL = DevServers.BASE_API_URL;
+global.THUMBNAIL_BASE_URL = DevServers.THUMBNAIL_BASE_URL;
