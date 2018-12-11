4201cd8d7cc8e5b98ba2dd63a2be2ba92f9a5aef
Megan Riel-Mehan <meganrm@gmail.com>
2018-12-10 19:18:07 -0800

    fixed download href

--- src/state/selection/selectors.ts
+++ src/state/selection/selectors.ts
@@ -273,7 +273,7 @@ export const getThumbnails = createSelector([
                 const cellLineId = cellData[CELL_LINE_NAME_KEY];
                 const fovId = cellData[FOV_ID_KEY];
                 const src = convertFileInfoToImgSrc(cellData);
-                const downloadHref = `${DOWNLOAD_URL_PREFIX}id=${convertFileInfoToAICSId(cellData)}`;
+                const downloadHref = `${DOWNLOAD_URL_PREFIX}&id=${convertFileInfoToAICSId(cellData)}`;
                 acc.push({
                     cellID,
                     downloadHref,
