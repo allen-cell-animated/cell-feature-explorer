031a4c4f69ead47b0221c46a6470885eeae98b20
Megan Riel-Mehan <meganrm@gmail.com>
2018-12-17 16:27:23 -0800

    search working and updated text style

--- src/containers/ThumbnailGallery/index.tsx
+++ src/containers/ThumbnailGallery/index.tsx
@@ -1,24 +1,33 @@
 import {
     Button,
+    Form,
     Icon,
+    Input,
     List,
 } from "antd";
+import { includes, map } from "lodash";
 import * as React from "react";
-import { connect } from "react-redux";
+import {
+    ActionCreator,
+    connect,
+} from "react-redux";
 
 import GalleryCard from "../../components/GalleryCard";
 import {
     clearAllSelectedPoints,
     deselectPoint,
-    selectCellFor3DViewer,
+    selectCellFor3DViewer, selectPoint,
 } from "../../state/selection/actions";
 import {
+    getClickedScatterPoints,
+    getIds,
     getSelected3DCell,
 } from "../../state/selection/selectors";
 import {
     DeselectPointAction,
     ResetSelectionAction,
     SelectCellIn3DAction,
+    SelectPointAction,
 } from "../../state/selection/types";
 import {
     State,
@@ -27,21 +36,67 @@ import {
 
 import { getThumbnails } from "./selectors";
 
+const Search = Input.Search;
+const FormItem = Form.Item;
+
 const styles = require("./style.css");
 
 interface ThumbnailGalleryProps {
+    clickedPoints: number[];
     data: Thumbnail[];
+    ids: string[];
     selectedCell: number;
+    addSearchedCell: ActionCreator<SelectPointAction>;
     handleClearAllSelectedPoints: () => ResetSelectionAction;
     handleDeselectPoint: (payload: number) => DeselectPointAction;
     handleOpenIn3D: (payload: number) => SelectCellIn3DAction;
 }
 
-class ThumbnailGallery extends React.Component<ThumbnailGalleryProps, {}> {
+interface ThumbnailGalleryState {
+    inputStatus: "success" | "error" | "warning" | "validating" | undefined;
+    message: string;
+}
+
+const initialState = {
+    inputStatus: undefined,
+    message: "",
+};
+
+class ThumbnailGallery extends React.Component<ThumbnailGalleryProps, ThumbnailGalleryState> {
 
     constructor(props: ThumbnailGalleryProps) {
         super(props);
         this.renderGalleryCard = this.renderGalleryCard.bind(this);
+        this.searchValidate = this.searchValidate.bind(this);
+        this.resetSearch = this.resetSearch.bind(this);
+        this.state = {
+            ...initialState,
+        };
+    }
+
+    public searchValidate(value: string) {
+        const { clickedPoints, ids, addSearchedCell } = this.props;
+        if (includes(map(clickedPoints, (ele) => ele.toString()), value)) {
+            return this.setState({
+                inputStatus: "warning",
+                message: "that cell is already in the gallery",
+            });
+        }
+        if (includes(ids, value)) {
+            addSearchedCell(value);
+            return this.setState({inputStatus: "success"});
+        }
+        this.setState({
+            inputStatus: "error",
+            message: "that id is not in our dataset",
+        });
+    }
+
+    public resetSearch() {
+        this.setState({
+            inputStatus: initialState.inputStatus,
+            message: initialState.message,
+        });
     }
 
     public render() {
@@ -53,12 +108,26 @@ class ThumbnailGallery extends React.Component<ThumbnailGalleryProps, {}> {
             <div id="gallery" className={styles.container}>
                 <section className={styles.galleryHeader}>
                     <h2><Icon type="picture"/>  Gallery</h2>
+                    <div>
+                        <FormItem
+                            hasFeedback={true}
+                            validateStatus={this.state.inputStatus}
+                            help={this.state.message}
+                        >
+                        <Search
+                            placeholder="search by cell id"
+                            onSearch={this.searchValidate}
+                            onChange={this.resetSearch}
+                        />
+                        </FormItem>
+                    </div>
                     {data.length > 0 ?
                         <Button
                             icon="close"
                             onClick={handleClearAllSelectedPoints}
                         >Clear All
                         </Button> : <h4>Clicked points on the plot will appear in this section</h4>}
+
                 </section>
                 <List
                     grid={{ gutter: 12, xs: 1, sm: 2, md: 4, lg: 6, xl: 12 }}
@@ -85,7 +154,7 @@ class ThumbnailGallery extends React.Component<ThumbnailGalleryProps, {}> {
                     title={item.labeledStructure}
                     handleDeselectPoint={handleDeselectPoint}
                     handleOpenIn3D={handleOpenIn3D}
-                    selected={Number(selectedCell) === Number(item.cellID)}
+                    selected={Number(selectedCell) === item.cellID}
                 />
             </List.Item>
         );
@@ -94,12 +163,15 @@ class ThumbnailGallery extends React.Component<ThumbnailGalleryProps, {}> {
 
 function mapStateToProps(state: State) {
     return {
+        clickedPoints: getClickedScatterPoints(state),
         data: getThumbnails(state),
+        ids: getIds(state),
         selectedCell: getSelected3DCell(state),
     };
 }
 
 const dispatchToPropsMap = {
+    addSearchedCell: selectPoint,
     handleClearAllSelectedPoints: clearAllSelectedPoints,
     handleDeselectPoint: deselectPoint,
     handleOpenIn3D: selectCellFor3DViewer,
