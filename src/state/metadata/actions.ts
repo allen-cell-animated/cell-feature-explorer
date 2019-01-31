9f9fd2fd8f96c9e63e918f14f82f67cbf9c4c0d0
Megan Riel-Mehan <meganrm@gmail.com>
2019-01-30 18:24:04 -0800

    fixed import statements

--- src/state/metadata/actions.ts
+++ src/state/metadata/actions.ts
@@ -3,14 +3,16 @@ import { AlbumData } from "../types";
 import {
     RECEIVE_ALBUM_DATA,
     RECEIVE_CELL_LINE_DATA,
-    RECEIVE_METADATA, REQUEST_ALBUM_DATA,
+    RECEIVE_METADATA,
+    REQUEST_ALBUM_DATA,
     REQUEST_CELL_LINE_DATA,
     REQUEST_FEATURE_DATA,
 } from "./constants";
 import {
     CellLineDef,
     MetaData,
-    ReceiveAction, ReceiveAlbumDataAction,
+    ReceiveAction,
+    ReceiveAlbumDataAction,
     ReceiveCellLineAction,
     RequestAction,
 } from "./types";
