afd6183d180041064610c9056e99c82b9aad6e3b
Gabe Medrash <gabeme@alleninstitute.org>
2019-02-07 09:29:41 -0800

    [ANIMCELLGR-217] Simplify problem considerably

--- src/index.tsx
+++ src/index.tsx
@@ -18,44 +18,30 @@ import {
 import { UrlState } from "./util";
 
 const pram = new Pram(createHistory());
-const initialURLParams = pram.getParams();
 const urlState = new UrlState();
-const store = createReduxStore({ selection: urlState.toAppState(initialURLParams)});
+const store = createReduxStore({ selection: urlState.toAppState(pram.getParams())});
 
 // in the case that this application is run within an iframe, setup communication between this app
 // and it host page to capture any URL search params that are intended to modify the state of this app
-const messenger = new MessageTarget(function updateLocalFrameURL(messageEvent: MessageEvent) {
-    pram.replaceParams(messageEvent.data);
+const messenger = new MessageTarget(function updateAppState(messageEvent: MessageEvent) {
+    store.dispatch(selection.actions.syncStateWithURL(messageEvent.data));
 });
 
-// when the URL changes, update app state
-let prevParams: any = initialURLParams;
-pram.onChange(function updateAppState(urlParams) {
-    if (urlState.urlParamsHaveChanged(prevParams, urlParams)) {
-        prevParams = urlParams;
-        try {
-            // if running in an iframe, keep host frame in-the-know about the state of the app
-            messenger.postMessage(urlParams);
-        } catch (error) {
-            // one common scenario in which this would fail is in dev/staging, in which this application
-            // is not being run inside an iframe.
-            // swallow error
-        } finally {
-            store.dispatch(selection.actions.syncStateWithURL(urlParams));
-        }
+// when the URL changes, notify host page if run within iframe
+pram.onChange(function notifyHostFrameOfURLChange(urlParams) {
+    try {
+        messenger.postMessage(urlParams);
+    } catch (error) {
+        // one common scenario in which this would fail is in dev/staging, in which this application
+        // is not being run inside an iframe.
+        // swallow error
     }
 });
 
 // keep the URL in sync with the state of this app
 store.subscribe(function updateURL() {
     const state = store.getState();
-
-    // setTimeout to force pram.replaceParams to run asynchronously. Otherwise it will call its change
-    // handler synchronously, forcing a store.dispatch within the call stack of this store.subscribe
-    setTimeout(() =>
-        pram.replaceParams(urlState.toUrlSearchParameterMap(state.selection)),
-        0
-    );
+    pram.replaceParams(urlState.toUrlSearchParameterMap(state.selection));
 });
 
 render(
