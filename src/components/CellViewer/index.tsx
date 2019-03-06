2479c8bdd7895cd5bf43cb3bbe275295337f753c
Megan Riel-Mehan <meganrm@gmail.com>
2019-03-06 10:16:03 -0800

    importing npm package viewer

--- src/components/CellViewer/index.tsx
+++ src/components/CellViewer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { CELL_VIEWER_URL } from "../../constants/index";
+import { ImageViewerApp } from "ac-3d-viewer";
 
 const styles = require("./style.css");
 
@@ -11,29 +12,20 @@ interface CellViewerProps {
 }
 
 const CellViewer: React.SFC<CellViewerProps> = ({ cellId, cellLineName, fovId }) => {
-    if (!cellId) {
+    if (!cellId || !cellLineName) {
         return null;
-    }
-    let query = "";
-    if (!fovId) {
-        // Assume cellId has the form:
-        // ?legacyName_1_2=2017_03_08_Struct_First_Pass_Seg/AICS-10/AICS-10_5_5
-        // After release, actual legacy links will look like : ?legacyName_1_2=AICS-10_5_5
-        // TODO this needs to be updated before release, when we flatten the directory
-        // structure of the bucket containing legacy version 1.2 images
-        query = `legacyName_1_2=${cellId}`;
-    } else {
-        query = `fovId=${fovId}&cellId=${cellId}&cellLine=${cellLineName}`;
     }
 
     return (
-            <iframe
-                id="three-d-viewer"
-                frameBorder={0}
-                className={styles.cellViewer}
-                src={`${CELL_VIEWER_URL}?${query}`}
-
+        <div className={styles.cellViewer}>
+            <ImageViewerApp
+                cellId={cellId}
+                cellLine={cellLineName}
+                fovId={fovId}
+                isLegacyName={!fovId}
             />
+
+        </div>
     );
 };
 
