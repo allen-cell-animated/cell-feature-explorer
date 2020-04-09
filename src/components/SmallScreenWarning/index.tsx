33f60a6b9492c5da402cb8b3125883eca846bace
meganrm <meganrm@gmail.com>
2020-04-09 14:35:15 -0700

    modal styling

--- src/components/SmallScreenWarning/index.tsx
+++ src/components/SmallScreenWarning/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Modal } from 'antd';
+import { Button, Modal } from 'antd';
+
+const styles = require('./style.css')
 
 interface SmallScreenWarningProps {
     handleOk: () => void;
@@ -8,17 +10,30 @@ interface SmallScreenWarningProps {
 
 const SmallScreenWarning: React.SFC<SmallScreenWarningProps> = ({ visible, handleOk }) => {
 
-    console.log('visible', visible);
-    return (<Modal
-        centered
-        title="Small Screen Warning"
-        visible={visible}
-        onOk={handleOk}
-    >
-        <p>Many features of this web tool are hidden while using it on a small screen.</p>
-        <p>To access all features, including a tool to plot measurements for over 30,000 cells, please visit this web page on a larger screen.
-</p>
-    </Modal>)
+    return (
+        <Modal
+            centered
+            title="Small Screen Warning"
+            visible={visible}
+            onOk={handleOk}
+            zIndex={4000}
+            footer={null}
+            maskStyle={{
+                background: '#000000d9'
+            }}
+            bodyStyle={{
+                backgroundColor: '#d8d8d8',
+                color: '#0a0a0a',
+            }}
+        >
+            <p>Many features of this web tool are hidden while using it on a small screen.</p>
+            <p>To access all features, including a tool to plot measurements for over 30,000 cells, please visit this web page on a larger screen.</p>
+            <div className={styles.buttonContainer}>
+
+                <Button className={styles.okButton} onClick={handleOk}>OK</Button>
+            </div>
+        </Modal>
+    )
 }
 
 export default SmallScreenWarning;
