55ee121e160878cffa36efe23b5029a461a2cb6b
meganrm <meganrm@gmail.com>
2020-04-10 15:59:16 -0700

    delinting

--- src/containers/ColorByMenu/selectors.ts
+++ src/containers/ColorByMenu/selectors.ts
@@ -52,7 +52,7 @@ const getColors = createSelector(
                 .map((ele: NumberOrString, index: number) =>
                     includes(filtersToExclude, ele) ? OFF_COLOR : proteinColors[index]) :
             proteinNames
-                .map((ele: NumberOrString,) =>
+                .map((ele: NumberOrString) =>
                     includes(filtersToExclude, ele) ? OFF_COLOR : DISABLE_COLOR);
     });
 
