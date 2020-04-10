237978f6a2d52f8d24b9652acadfb156f1a8a71e
meganrm <meganrm@gmail.com>
2020-04-09 17:48:00 -0700

    delinting

--- src/components/SmallScreenWarning/index.tsx
+++ src/components/SmallScreenWarning/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Button, Modal, Checkbox } from 'antd';
-import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { Button, Checkbox, Modal } from "antd";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
+import React from "react";
 
-const styles = require('./style.css')
+const styles = require("./style.css");
 
 interface SmallScreenWarningProps {
     handleOk: () => void;
@@ -11,25 +11,28 @@ interface SmallScreenWarningProps {
 }
 
 const SmallScreenWarning: React.SFC<SmallScreenWarningProps> = ({ visible, handleOk, onDismissCheckboxChecked }) => {
-    const onCheckboxChange = ({ target }: CheckboxChangeEvent) => onDismissCheckboxChecked(target.checked)
+    const onCheckboxChange = ({ target }: CheckboxChangeEvent) => onDismissCheckboxChecked(target.checked);
     return (
         <Modal
-            centered
+            centered={true}
             title="Small Screen Warning"
             visible={visible}
             onOk={handleOk}
             zIndex={4000}
             footer={null}
             maskStyle={{
-                background: '#000000d9'
+                background: "#000000d9",
             }}
             bodyStyle={{
-                backgroundColor: '#d8d8d8',
-                color: '#0a0a0a',
+                backgroundColor: "#d8d8d8",
+                color: "#0a0a0a",
             }}
         >
             <p>Many features of this web tool are hidden while using it on a small screen.</p>
-            <p>To access all features, including a tool to plot measurements for over 30,000 cells, please visit this web page on a larger screen.</p>
+            <p>
+                To access all features, including a tool to plot measurements for over 30,000 cells,{" "}
+                please visit this web page on a larger screen.
+            </p>
             <div className={styles.checkboxContainer}>
                 <Checkbox onChange={onCheckboxChange}>Don't show this message again.</Checkbox>
             </div>
@@ -37,7 +40,7 @@ const SmallScreenWarning: React.SFC<SmallScreenWarningProps> = ({ visible, handl
                 <Button className={styles.okButton} onClick={handleOk}>OK</Button>
             </div>
         </Modal>
-    )
-}
+    );
+};
 
 export default SmallScreenWarning;
