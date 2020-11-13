2e105b38ec64bf7cdf38eb398013e9290c40c908
meganrm <meganrm@gmail.com>
2020-11-13 12:55:03 -0800

    fixed error

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -137,7 +137,7 @@ module.exports = ({ analyze, env } = {}) => ({
     },
     output: {
         path: path.resolve(__dirname, '../', 'dist'),
-        filename: '[name].[chunkhash].js'
+        filename: '[name].[hash].js'
     },
     plugins: getPluginsByEnv(env, analyze),
     resolve: {
