f1cf220ae423bf8a96691be6306774cb608a6b6e
meganrm <meganrm@gmail.com>
2020-12-01 22:23:24 -0800

    replaced strings with consts

--- src/state/image-dataset/firebase/index.ts
+++ src/state/image-dataset/firebase/index.ts
@@ -23,6 +23,12 @@ import { ImageDataset } from "../types";
 
 import { firestore } from "./configure-firebase";
 
+const CFE_DATASETS = "cfe-datasets";
+const VERSION = "v2";
+const CELL_FILE_INFO_COLLECTION = "cell-file-info";
+const CELL_FEATURES_COLLECTION = "measured-features-values";
+const CELL_FEATURE_DEFS_COLLECTION = "measured-features-names";
+
 class FirebaseRequest implements ImageDataset {
     private collectionRef: DocumentReference;
     private featureDefs: string;
@@ -133,17 +139,17 @@ class FirebaseRequest implements ImageDataset {
     };
 
     public getFileInfoByCellId = (cellId: string) => {
-        return this.getDoc("cell-file-info", cellId).then((doc) => doc.data());
+        return this.getDoc(CELL_FILE_INFO_COLLECTION, cellId).then((doc) => doc.data());
     };
 
     public getFileInfoByArrayOfCellIds = (cellIds: string[]) => {
         Promise.all(cellIds.map((id) => {
-            return this.getDoc("cell-file-info", id).then((doc) => doc.data());
+            return this.getDoc(CELL_FILE_INFO_COLLECTION, id).then((doc) => doc.data());
         }))
     };
 
     public getPageOfFeatureData = async (lastVisible: QueryDocumentSnapshot) => {
-        const snapshot = await this.getPage("measured-features-values", 10000, lastVisible);
+        const snapshot = await this.getPage(CELL_FEATURES_COLLECTION, 10000, lastVisible);
         if (!snapshot.empty) {
             const dataset: MetadataStateBranch = {
                 [ARRAY_OF_CELL_IDS_KEY]: [],
@@ -169,7 +175,7 @@ class FirebaseRequest implements ImageDataset {
     };
 
     public getFeatureData = () => {
-        return this.getCollection("measured-features-values", 10000).then(
+        return this.getCollection(CELL_FEATURES_COLLECTION, 10000).then(
             (snapshot: QuerySnapshot) => {
                 if (!snapshot.empty) {
                     const dataset: MetadataStateBranch = {
@@ -194,7 +200,7 @@ class FirebaseRequest implements ImageDataset {
     };
 
     public getMeasuredFeatureNames = async () => {
-        const snapshot = await this.getCollection("measured-features-names");
+        const snapshot = await this.getCollection(CELL_FEATURE_DEFS_COLLECTION);
         const dataset: MeasuredFeatureDef[] = [];
         snapshot.forEach((doc: QueryDocumentSnapshot) => {
             dataset.push(doc.data() as MeasuredFeatureDef);
