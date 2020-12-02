ca6df8905b3fd9b6ce98dcc390dfaff87463de1b
meganrm <meganrm@gmail.com>
2020-12-01 22:17:25 -0800

    styling progress bar

--- src/state/metadata/selectors.ts
+++ src/state/metadata/selectors.ts
@@ -1,4 +1,4 @@
-import { map, filter, sortBy, isEmpty } from "lodash";
+import { map, filter, sortBy, isEmpty, toInteger } from "lodash";
 import { createSelector } from "reselect";
 
 import {
@@ -27,7 +27,7 @@ export const getPlotLoadingProgress = createSelector([getMeasuredFeatureValues],
     if (isEmpty(measuredFeatures)) {
         return 0;
     }
-    return measuredFeatures[ARRAY_OF_CELL_IDS_KEY].length/TOTAL_CELLS * 100;
+    return toInteger(measuredFeatures[ARRAY_OF_CELL_IDS_KEY].length/TOTAL_CELLS * 100);
 })
 
 export const getSortedCellLineDefs = createSelector([getCellLineDefs], (cellLineDefs: CellLineDef[]): CellLineDef[] =>
