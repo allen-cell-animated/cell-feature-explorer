f0536c99ca4bb3cd9c1eb34f61e4189aaf0ca912
Dan <danielt@alleninstitute.org>
2021-03-29 10:11:52 -0700

    auto formatting; rename USE_GITHUB_FOR_DATASET to USE_JSON_DATASET

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,83 +1,81 @@
-const path = require('path');
+const path = require("path");
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-const Env = require('./constants').Env;
+const Env = require("./constants").Env;
 
 const BASE_PLUGINS = [
     new webpack.DefinePlugin({
-        'process.env.USE_GITHUB_FOR_DATASET': JSON.stringify(process.env.USE_GITHUB_FOR_DATASET) || false,
+        "process.env.USE_JSON_DATASET": JSON.stringify(process.env.USE_JSON_DATASET) || false,
     }),
     new ForkTsCheckerWebpackPlugin({
-        tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
+        tsconfig: path.resolve(__dirname, "../", "tsconfig.json"),
         workers: ForkTsCheckerWebpackPlugin.TWO_CPUS_FREE,
     }),
-    new CleanWebpackPlugin(['dist'], {
-        root: path.resolve(__dirname, '../'),
+    new CleanWebpackPlugin(["dist"], {
+        root: path.resolve(__dirname, "../"),
         watch: true,
     }),
-    new CopyWebpackPlugin(
-        [
-            {
-                from: path.resolve(__dirname, '../src/data', 'cell_feature_analysis.json'),
-                to: 'data'
-            },
-            {
-                from: path.resolve(__dirname, '../src/data', 'feature_defs.json'),
-                to: 'data'
-            },
-            {
-                from: path.resolve(__dirname, '../src/data', 'cell-line-def.json'),
-                to: 'data'
-            },
-            {
-                from: path.resolve(__dirname, '../src/data', 'albums.json'),
-                to: 'data'
-            },
-            {
-                from: path.resolve(__dirname, '../src/images'),
-                to: 'images'
-            }
-        ]
-    ),
+    new CopyWebpackPlugin([
+        {
+            from: path.resolve(__dirname, "../src/data", "cell_feature_analysis.json"),
+            to: "data",
+        },
+        {
+            from: path.resolve(__dirname, "../src/data", "feature_defs.json"),
+            to: "data",
+        },
+        {
+            from: path.resolve(__dirname, "../src/data", "cell-line-def.json"),
+            to: "data",
+        },
+        {
+            from: path.resolve(__dirname, "../src/data", "albums.json"),
+            to: "data",
+        },
+        {
+            from: path.resolve(__dirname, "../src/images"),
+            to: "images",
+        },
+    ]),
     new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
+        filename: "style.[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.template.html')
-    })
+        template: path.resolve(__dirname, "index.template.html"),
+    }),
 ];
 
-const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: 'static' })];
+const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: "static" })];
 
 const PLUGINS_BY_ENV = {
     [Env.PRODUCTION]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
+            "process.env.NODE_ENV": JSON.stringify("production"),
         }),
-        new webpack.HashedModuleIdsPlugin()
+        new webpack.HashedModuleIdsPlugin(),
     ],
     [Env.STAGE]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('staging'),
+            "process.env.NODE_ENV": JSON.stringify("staging"),
         }),
-        new webpack.NamedModulesPlugin()
+        new webpack.NamedModulesPlugin(),
     ],
     [Env.DEVELOPMENT]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('dev'),
+            "process.env.NODE_ENV": JSON.stringify("dev"),
         }),
-    ]
+    ],
 };
 
 module.exports = (env, analyzer) => [
     ...BASE_PLUGINS,
     ...(analyzer ? BUNDLE_ANALYZER : []),
-    ...(PLUGINS_BY_ENV[env] || [])
+    ...(PLUGINS_BY_ENV[env] || []),
 ];
