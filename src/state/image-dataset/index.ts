70d697f6d73de514164bb59ae280f7cee62c2f7a
Dan <danielt@alleninstitute.org>
2021-03-29 13:42:15 -0700

    somehow missed saving this code change

--- src/state/image-dataset/index.ts
+++ src/state/image-dataset/index.ts
@@ -5,5 +5,5 @@ import { ImageDataset } from "./types";
 // by default will use Firebase for dataset, can be switched to github using ENV
 // variable
 export default function RequestClassToUse(): ImageDataset {
-    return process.env.USE_GITHUB_FOR_DATASET ? new JsonRequest() : new FirebaseRequest();
+    return process.env.USE_JSON_DATASET ? new JsonRequest() : new FirebaseRequest();
 }
