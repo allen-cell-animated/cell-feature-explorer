0a081573c3a960260c8526ebc4313c0d0cd454bc
Na Hyung Choi <schoinh@gmail.com>
2021-10-05 14:10:42 -0700

    Show dataset name if title isn't present

--- src/components/DatasetCard/index.tsx
+++ src/components/DatasetCard/index.tsx
@@ -11,6 +11,7 @@ interface DatasetCardProps extends DatasetMetaData {
 
 const DatasetCard: React.FunctionComponent<DatasetCardProps> = ({
     title,
+    name,
     version,
     description,
     handleSelectDataset,
@@ -21,12 +22,12 @@ const DatasetCard: React.FunctionComponent<DatasetCardProps> = ({
 }: DatasetCardProps) => {
     // Prefix version number with a "v" if necessary
     const versionPrefixed = version.startsWith("v") ? version : `v${version}`;
-    
+
     const displayTitle = (
         <>
             <div>
                 {userData.isNew && <Tag color="purple">beta</Tag>}
-                {title}
+                {title ? title : name}
             </div>
             <span className={styles.version}>{versionPrefixed}</span>
             {userData.inReview && (
