cffac297c55946790e70ae5d3bb4f52bf17c5846
Na Hyung Choi <schoinh@gmail.com>
2022-01-18 15:38:53 -0800

    Change order of interface definitions

--- src/state/image-dataset/types.ts
+++ src/state/image-dataset/types.ts
@@ -1,7 +1,7 @@
+import { ViewerChannelSettings } from "@aics/web-3d-viewer/type-declarations";
+
 import { CellLineDef, DataForPlot, FileInfo, MeasuredFeatureDef } from "../metadata/types";
 import { Album } from "../types";
-
-import { ViewerChannelSettings } from "@aics/web-3d-viewer/type-declarations";
 
 export interface InitialDatasetSelections {
     defaultXAxis: string;
@@ -10,18 +10,6 @@ export interface InitialDatasetSelections {
     thumbnailRoot: string;
     downloadRoot: string;
     volumeViewerDataRoot: string;
-}
-
-export interface ImageDataset {
-    selectDataset(manifest: any): Promise<InitialDatasetSelections>;
-    getAvailableDatasets(): Promise<Megaset[]>;
-    getCellLineDefs(): Promise<CellLineDef[]>;
-    getViewerChannelSettings(): Promise<ViewerChannelSettings>;
-    getFeatureData(): Promise<DataForPlot>;
-    getAlbumData(): Promise<Album[]>;
-    getMeasuredFeatureDefs(): Promise<MeasuredFeatureDef[]>;
-    getFileInfoByCellId(id: string): Promise<FileInfo | undefined>;
-    getFileInfoByArrayOfCellIds(ids: string[]): Promise<(FileInfo | undefined)[]>;
 }
 
 export interface DatasetMetaData {
@@ -57,4 +45,16 @@ export interface Megaset {
         [key: string]: DatasetMetaData
     };
     publications?: Publication[];
+}
+
+export interface ImageDataset {
+    selectDataset(manifest: any): Promise<InitialDatasetSelections>;
+    getAvailableDatasets(): Promise<Megaset[]>;
+    getCellLineDefs(): Promise<CellLineDef[]>;
+    getViewerChannelSettings(): Promise<ViewerChannelSettings>;
+    getFeatureData(): Promise<DataForPlot>;
+    getAlbumData(): Promise<Album[]>;
+    getMeasuredFeatureDefs(): Promise<MeasuredFeatureDef[]>;
+    getFileInfoByCellId(id: string): Promise<FileInfo | undefined>;
+    getFileInfoByArrayOfCellIds(ids: string[]): Promise<(FileInfo | undefined)[]>;
 }
