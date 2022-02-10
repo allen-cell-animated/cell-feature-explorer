d63b53012ba09ba266dccb1ba5bde22b4a652710
Na Hyung Choi <schoinh@gmail.com>
2022-02-09 16:52:47 -0800

    Feature/megacards (#89)
    * Start grouping datasets together
    
    * Start adding publications section
    
    * Render megaset title
    
    * Display publication info, albeit ugly
    
    * Give axis dropdown arrow more space on left
    
    * Add and style Load button
    
    * Clean up unused imports
    
    * Change title on landing page
    
    * Style some titles and margins
    
    * Add some styling to publications
    
    * Make links blue
    
    * Style "new" tag and adjust purple color
    
    * Use JS to prevent publications from widening megacard
    
    * Add comment about maxWidth
    
    * Make dataset cards fill available width
    
    * Add spacer to make dataset card heights even
    
    * Successfully wrap megacards
    
    * Make datasets wrap inside a megacard
    
    * Make all megacards one-column when screen is not large
    
    * Make page header wrap properly
    
    * Make caption for dataset section more mobile friendly
    
    * Make everything mobile-friendly
    
    * Remove unsued imports
    
    * Clean up
    
    * Use a constant for dataset card width
    
    * Fix some formatting
    
    * Add comment about purple color
    
    * Use color name variable for #ffffff
    
    * Fix whitespace in bottom section
    
    * Use CSS variables for widths
    
    * Replace width value with CSS variable in one last place

---
+++ src/components/MegasetCard/index.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { map } from "lodash";
+
+import { Megaset } from "../../state/image-dataset/types";
+import DatasetCard from "../../components/DatasetCard";
+
+import styles from "./style.css";
+
+interface MegasetCardProps {
+    key: string;
+    megaset: Megaset;
+    handleSelectDataset: (id: string) => void;
+}
+
+/* 
+Mirroring our data structure, every dataset card is rendered in a MegasetCard,
+but a MegasetCard can contain one or multiple dataset cards.
+*/
+const MegasetCard: React.FunctionComponent<MegasetCardProps> = ({
+    megaset,
+    handleSelectDataset,
+}: MegasetCardProps) => {
+    const computedStyle = getComputedStyle(document.body);
+    const datasetCardWidth = computedStyle.getPropertyValue("--dataset-card-width");
+    const numDatasets = Object.keys(megaset.datasets).length;
+    // Set a max-width for the container so that publications shrink into the 
+    // container instead of the container expanding to fit publications
+    const maxWidth = numDatasets > 1 ? "100%" : datasetCardWidth;
+
+    return (
+        <div 
+            key={megaset.name}
+            className={styles.container}
+            style={{ maxWidth: maxWidth }}
+        >
+            {numDatasets > 1 && 
+                <div className={styles.megasetTitle}>{megaset.title}</div>
+            }
+            <div className={styles.datasetCards}>
+                {map(megaset.datasets, (dataset) => (
+                    <DatasetCard key={dataset.id} {...dataset} handleSelectDataset={handleSelectDataset} />
+                ))}
+            </div>
+            {megaset.publications && megaset.publications.length > 0 && 
+                <div className={styles.publications}>
+                    Publications
+                    {megaset.publications.map(publication => (
+                        <div key={`${megaset.name}-${publication.title}`} className={styles.publication}>
+                            <div className={styles.publicationTitle}>
+                                <a href={publication.url} target="blank">
+                                    {publication.title}
+                                </a>
+                            </div>
+                            <div className={styles.journalDate}>
+                                {publication.citation}.
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            }
+        </div>
+    )
+};
+
+export default MegasetCard;
