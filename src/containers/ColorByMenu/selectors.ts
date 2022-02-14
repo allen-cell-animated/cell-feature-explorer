462f6de7d5c059598a639bcb449d0e003e684590
meganrm <meganrm@gmail.com>
2022-02-14 13:15:21 -0800

    remove ternary statements

--- src/containers/ColorByMenu/selectors.ts
+++ src/containers/ColorByMenu/selectors.ts
@@ -83,11 +83,12 @@ const getColorForCategory = (
 ) => {
     if (isDisabled) {
         return DISABLE_COLOR;
-    }
-    if (showGroupByColors) {
-        return isExcluded ? OFF_COLOR : categoryColor;
+    } else if (isExcluded) {
+        return OFF_COLOR;
+    } else if (showGroupByColors) {
+        return categoryColor;
     } else {
-        return isExcluded ? OFF_COLOR : DISABLE_COLOR;
+        return DISABLE_COLOR;
     }
 };
 
