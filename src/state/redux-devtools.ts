268af2e6d10a2970e33566a2bd3773c4a6545ca2
Cameron Fraser <cameron.fraser@alleninstitute.org>
2022-12-21 17:13:18 -0800

    devtools sanitizes batched actions too

--- src/state/redux-devtools.ts
+++ src/state/redux-devtools.ts
@@ -1,11 +1,12 @@
 import { mapValues } from "lodash";
+import { BATCH_ACTIONS } from "./util";
 import { RECEIVE_DATA_FOR_PLOT } from "./metadata/constants";
 import { MetadataStateBranch } from "./metadata/types";
 import { AnyAction, compose } from "redux";
 import { State } from ".";
 
-// Redux DevTools chugs hard if forced to save large arrays of feature data over and over again
-// The point of (most of) this file is to ensure it doesn't have to
+// Redux DevTools runs unusably slowly if forced to save large arrays of feature data over and over again
+// The point of this file is to ensure it doesn't have to
 
 const SUMMARY_LENGTH = 5;
 
@@ -16,7 +17,7 @@ const summarizeLongArray = <T>(arr: T[]): SummarizedArray<T> => {
         slice.push(`...and ${arr.length - SUMMARY_LENGTH} more`);
     }
     return slice;
-}
+};
 
 const summarizeFeatureData = (data: MetadataStateBranch["featureData"]) => ({
     indices: summarizeLongArray(data.indices),
@@ -24,19 +25,24 @@ const summarizeFeatureData = (data: MetadataStateBranch["featureData"]) => ({
     labels: mapValues(data.labels, summarizeLongArray),
 });
 
-const actionSanitizer = (action: AnyAction) => (
-    action.type === RECEIVE_DATA_FOR_PLOT ? { ...action, payload: summarizeFeatureData(action.payload) } : action
-);
+const actionSanitizer = (action: AnyAction) => {
+    if (action.type === RECEIVE_DATA_FOR_PLOT) {
+        return { ...action, payload: summarizeFeatureData(action.payload) };
+    } else if (action.type === BATCH_ACTIONS) {
+        return { ...action, payload: action.payload.map(actionSanitizer) };
+    }
+    return action;
+};
+
 const stateSanitizer = (state: State) => ({
     ...state,
     metadata: {
         ...state.metadata,
         featureData: summarizeFeatureData(state.metadata.featureData),
-    }
+    },
 });
 
-const devtoolsCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as (opts: any) => typeof compose)({ actionSanitizer, stateSanitizer })
-    : compose;
+type DevtoolsComposeType = ((opts: any) => typeof compose) | undefined;
+const devtoolsCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as DevtoolsComposeType;
 
-export default devtoolsCompose;
+export default devtoolsCompose ? devtoolsCompose({ actionSanitizer, stateSanitizer }) : compose;
