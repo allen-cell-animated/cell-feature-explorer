ff5bf8af50ce2f950dfac8e8124ddfa1b865b6a7
Peyton Lee <peyton.lee@alleninstitute.org>
2025-11-12 16:34:26 -0800

    refactor: Remove ViewerStateProvider, set viewerSettings directly

--- src/components/CellViewer/index.tsx
+++ src/components/CellViewer/index.tsx
@@ -1,4 +1,4 @@
-import { ImageViewerApp, ViewerStateProvider } from "@aics/vole-app";
+import { ImageViewerApp } from "@aics/vole-app";
 import React from "react";
 
 import styles from "./style.css";
@@ -19,22 +19,21 @@ const CellViewer: React.FunctionComponent<VolumeViewerProps> = (props) => {
 
     return (
         <div className={styles.cellViewerContainer} style={CONTAINER_STYLE}>
-            <ViewerStateProvider viewerSettings={props.viewerSettings}>
-                <ImageViewerApp
-                    cellId={props.cellId}
-                    imageUrl={props.baseUrl + props.cellPath}
-                    imageDownloadHref={props.cellDownloadHref}
-                    parentImageUrl={props.fovPath ? props.baseUrl + props.fovPath : ""}
-                    parentImageDownloadHref={props.fovDownloadHref}
-                    viewerChannelSettings={props.viewerChannelSettings}
-                    transform={props.transform}
-                    onControlPanelToggle={props.onControlPanelToggle}
-                    metadata={props.metadata}
-                    appHeight="100%"
-                    canvasMargin="0 0 0 0"
-                    {...props.appProps}
-                />
-            </ViewerStateProvider>
+            <ImageViewerApp
+                cellId={props.cellId}
+                imageUrl={props.baseUrl + props.cellPath}
+                imageDownloadHref={props.cellDownloadHref}
+                parentImageUrl={props.fovPath ? props.baseUrl + props.fovPath : ""}
+                parentImageDownloadHref={props.fovDownloadHref}
+                viewerChannelSettings={props.viewerChannelSettings}
+                transform={props.transform}
+                onControlPanelToggle={props.onControlPanelToggle}
+                metadata={props.metadata}
+                appHeight="100%"
+                canvasMargin="0 0 0 0"
+                {...props.appProps}
+                viewerSettings={props.viewerSettings}
+            />
         </div>
     );
 };
