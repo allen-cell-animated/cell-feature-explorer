bff5522e959ffd8acbaacdb8928f4cbec41f43aa
Peyton Lee <peyton.lee@alleninstitute.org>
2025-11-17 09:05:01 -0800

    refactor: Remove unused import

--- src/util/UrlState.ts
+++ src/util/UrlState.ts
@@ -31,7 +31,6 @@ import {
 } from "../state/selection/actions";
 import { initialState } from "../state/selection/reducer";
 import type { SelectionStateBranch } from "../state/selection/types";
-import { CSV_DATASET_NAME } from "../state/selection/constants";
 
 export enum URLSearchParam {
     cellSelectedFor3D = "cellSelectedFor3D",
