79aa33e749d90875c3797b5389fb5cd4485ca87a
meganrm <meganrm@gmail.com>
2026-02-25 14:02:40 -0800

    dummy data force re-render

--- src/components/CellViewer/index.tsx
+++ src/components/CellViewer/index.tsx
@@ -1,38 +1,79 @@
 import { ImageViewerApp, ViewerStateProvider } from "@aics/vole-app";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./style.css";
 
 import { VolumeViewerProps } from "../../containers/Cfe/selectors";
 
-const CONTAINER_STYLE = {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "red",
+const FORCE_RERENDER_DELAY_MS = 1000;
+
+const INITIAL_DUMMY_PROPS: VolumeViewerProps = {
+    cellId: "103437",
+    cellPath: "AICS-57/AICS-57_13104_103437_atlas.json",
+    cellDownloadHref:
+        "https://files.allencell.org/api/2.0/file/download?collection=cellviewer-1-4&id=C103437",
+    fovDownloadHref:
+        "https://files.allencell.org/api/2.0/file/download?collection=cellviewer-1-4&id=F13104",
+    fovPath: "AICS-57/AICS-57_13104_atlas.json",
+    baseUrl:
+        "https://s3-us-west-2.amazonaws.com/bisque.allencell.org/v1.4.0/Cell-Viewer_Thumbnails/",
+    viewerSettings: {},
+    transform: undefined,
+    metadata: undefined,
+    appProps: {},
 };
 
 const CellViewer: React.FunctionComponent<VolumeViewerProps> = (props) => {
+
+    ///** There is a bug in the viewer where when it mounts with initial data, it doesn't show anything
+    // however, switching between data works correctly
+    // this temporary code makes sure the viewer always shows data, but lines 31-51 should be deleted 
+    // once the bug is fixed */
+    const [prevCellId, setPrevCellId] = useState<string | undefined>(props.cellId);
+
+    const [propsToUse, setPropsToUse] = useState(INITIAL_DUMMY_PROPS);
+    useEffect(() => {
+        setPrevCellId(props.cellId);
+    }, [props.cellId]);
+
+
+    useEffect(() => {
+        if (!props.cellId) {
+            return;
+        }
+        if (prevCellId === undefined) {
+            const timeout = setTimeout(() => {
+                setPropsToUse(props);
+            }, FORCE_RERENDER_DELAY_MS);
+
+            return () => clearTimeout(timeout);
+        } else {
+            setPropsToUse(props);
+        }
+    }, [props.cellId]);
+
     if (!props.cellId) {
-        return null;
+        return <div className={styles.emptyOverlay}>Select a cell in gallery to view it in 3D</div>;
     }
 
     return (
-        <div className={styles.cellViewerContainer} style={CONTAINER_STYLE}>
-            <ViewerStateProvider viewerSettings={props.viewerSettings}>
+        <div className={styles.cellViewerContainer}>
+            <ViewerStateProvider viewerSettings={propsToUse.viewerSettings}>
                 <ImageViewerApp
-                    cellId={props.cellId}
-                    imageUrl={props.baseUrl + props.cellPath}
-                    imageDownloadHref={props.cellDownloadHref}
-                    parentImageUrl={props.fovPath ? props.baseUrl + props.fovPath : ""}
-                    parentImageDownloadHref={props.fovDownloadHref}
-                    viewerChannelSettings={props.viewerChannelSettings}
-                    transform={props.transform}
-                    onControlPanelToggle={props.onControlPanelToggle}
-                    metadata={props.metadata}
+                    cellId={propsToUse.cellId}
+                    imageUrl={propsToUse.baseUrl + propsToUse.cellPath}
+                    imageDownloadHref={propsToUse.cellDownloadHref}
+                    parentImageUrl={
+                        propsToUse.fovPath ? propsToUse.baseUrl + propsToUse.fovPath : ""
+                    }
+                    parentImageDownloadHref={propsToUse.fovDownloadHref}
+                    viewerChannelSettings={propsToUse.viewerChannelSettings}
+                    transform={propsToUse.transform}
+                    onControlPanelToggle={propsToUse.onControlPanelToggle}
+                    metadata={propsToUse.metadata}
                     appHeight="100%"
                     canvasMargin="0 0 0 0"
-                    {...props.appProps}
+                    {...propsToUse.appProps}
                 />
             </ViewerStateProvider>
         </div>
