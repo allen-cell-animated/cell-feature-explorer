17edf6ea43e82e882e7e2ff46a4d3b89d21f4b7d
Peyton Lee <peytonalee@gmail.com>
2026-04-21 09:14:51 -0700

    refactor: Ran formatting on UrlState (colors pt. 2) (#288)
    * fix: Keep counts consistent

--- src/util/UrlState.ts
+++ src/util/UrlState.ts
@@ -9,17 +9,11 @@ import {
     map,
     mergeWith,
     reduce,
-    isEqual
+    isEqual,
 } from "lodash";
 import type { Action } from "redux";
 
-import {
-    CELL_ID_KEY,
-    COLOR_BY_SELECTOR,
-    GROUP_BY_KEY,
-    X_AXIS_ID,
-    Y_AXIS_ID,
-} from "../constants";
+import { CELL_ID_KEY, COLOR_BY_SELECTOR, GROUP_BY_KEY, X_AXIS_ID, Y_AXIS_ID } from "../constants";
 import type { FileInfo } from "../state/metadata/types";
 import {
     changeAxis,
@@ -55,11 +49,17 @@ export interface URLSearchParamMap {
 }
 
 interface URLSearchParamToActionCreatorMap {
-    [index: string]: (value: URLSearchParamValue, collection: URLSearchParamMap) => Action | Action[];
+    [index: string]: (
+        value: URLSearchParamValue,
+        collection: URLSearchParamMap
+    ) => Action | Action[];
 }
 
 interface URLSearchParamToStateMap {
-    [index: string]: (value: URLSearchParamValue, collection: URLSearchParamMap) => { [key: string]: StateValue };
+    [index: string]: (
+        value: URLSearchParamValue,
+        collection: URLSearchParamMap
+    ) => { [key: string]: StateValue };
 }
 
 interface StateToUrlSearchParamMap {
@@ -69,68 +69,92 @@ interface StateToUrlSearchParamMap {
 export default class UrlState {
     public static toAppState(searchParameterMap: URLSearchParamMap): Partial<SelectionStateBranch> {
         const initial: Partial<SelectionStateBranch> = {};
-        return reduce(searchParameterMap, (accum, searchParamValue, searchParamKey) => {
-           if (UrlState.urlParamToStateMap.hasOwnProperty(searchParamKey)) {
-               const result = UrlState.urlParamToStateMap[searchParamKey](searchParamValue, searchParameterMap);
-               return mergeWith({}, accum, result, (accumValue, resultValue) => {
-                   if (Array.isArray(accumValue)) {
-                       return accumValue.concat(resultValue);
-                   }
-               });
-           }
-           return accum;
-        }, initial);
+        return reduce(
+            searchParameterMap,
+            (accum, searchParamValue, searchParamKey) => {
+                if (UrlState.urlParamToStateMap.hasOwnProperty(searchParamKey)) {
+                    const result = UrlState.urlParamToStateMap[searchParamKey](
+                        searchParamValue,
+                        searchParameterMap
+                    );
+                    return mergeWith({}, accum, result, (accumValue, resultValue) => {
+                        if (Array.isArray(accumValue)) {
+                            return accumValue.concat(resultValue);
+                        }
+                    });
+                }
+                return accum;
+            },
+            initial
+        );
     }
 
-    public static paramChanged(key: string, newParams: URLSearchParamMap, currentParams: URLSearchParamMap, ): boolean {
+    public static paramChanged(
+        key: string,
+        newParams: URLSearchParamMap,
+        currentParams: URLSearchParamMap
+    ): boolean {
         return !isEqual(currentParams[key], newParams[key]);
     }
 
-    public static paramsChanged(newParamsFromState: URLSearchParamMap, currentParams: URLSearchParamMap): boolean {
-        return !isEqual(currentParams, newParamsFromState) 
+    public static paramsChanged(
+        newParamsFromState: URLSearchParamMap,
+        currentParams: URLSearchParamMap
+    ): boolean {
+        return !isEqual(currentParams, newParamsFromState);
     }
 
     public static toReduxActions(searchParameterMap: URLSearchParamMap): Action[] {
         const initial: Action[] = [];
-        return reduce(searchParameterMap, (accum, searchParamValue, searchParamKey) => {
-            if (UrlState.urlParamToActionCreatorMap.hasOwnProperty(searchParamKey)) {
-                const action = UrlState.urlParamToActionCreatorMap[searchParamKey](
-                    searchParamValue,
-                    searchParameterMap
-                );
+        return reduce(
+            searchParameterMap,
+            (accum, searchParamValue, searchParamKey) => {
+                if (UrlState.urlParamToActionCreatorMap.hasOwnProperty(searchParamKey)) {
+                    const action = UrlState.urlParamToActionCreatorMap[searchParamKey](
+                        searchParamValue,
+                        searchParameterMap
+                    );
 
-                if (action) {
-                    return [...accum, ...castArray(action)];
+                    if (action) {
+                        return [...accum, ...castArray(action)];
+                    }
                 }
-            }
-            return accum;
-        }, initial);
+                return accum;
+            },
+            initial
+        );
     }
 
-    public static toUrlSearchParameterMap(selections: Partial<SelectionStateBranch>): URLSearchParamMap {
+    public static toUrlSearchParameterMap(
+        selections: Partial<SelectionStateBranch>
+    ): URLSearchParamMap {
         const initial: URLSearchParamMap = {};
-        return reduce(selections, (accum, selectionStateValue, selectionStateKey) => {
-            if (
-                UrlState.stateToUrlParamMap.hasOwnProperty(selectionStateKey) &&
-                UrlState.valueIsMeaningfulToSerialize(selectionStateValue) &&
-                UrlState.valueIsNotDefault(selectionStateValue, selectionStateKey)
-            ) {
-                return {
-                    ...accum,
-                    ...this.stateToUrlParamMap[selectionStateKey](selectionStateValue),
-                };
-            }
-            return accum;
-        }, initial);
+        return reduce(
+            selections,
+            (accum, selectionStateValue, selectionStateKey) => {
+                if (
+                    UrlState.stateToUrlParamMap.hasOwnProperty(selectionStateKey) &&
+                    UrlState.valueIsMeaningfulToSerialize(selectionStateValue) &&
+                    UrlState.valueIsNotDefault(selectionStateValue, selectionStateKey)
+                ) {
+                    return {
+                        ...accum,
+                        ...this.stateToUrlParamMap[selectionStateKey](selectionStateValue),
+                    };
+                }
+                return accum;
+            },
+            initial
+        );
     }
 
     private static urlParamToActionCreatorMap: URLSearchParamToActionCreatorMap = {
         [URLSearchParam.cellSelectedFor3D]: (cellId, params) => {
-            const selectCellFor3DAction = selectCellFor3DViewer({ id: String(cellId)});
+            const selectCellFor3DAction = selectCellFor3DViewer({ id: String(cellId) });
 
             // add this cell to the list of selected points if it does not already exist
             if (!includes(castArray(params[URLSearchParam.selectedPoint]), cellId)) {
-                return [selectPoint({id: String(cellId)}), selectCellFor3DAction];
+                return [selectPoint({ id: String(cellId) }), selectCellFor3DAction];
             }
 
             return selectCellFor3DAction;
@@ -139,15 +163,18 @@ export default class UrlState {
         [URLSearchParam.colorBy]: (colorBy) => changeAxis(COLOR_BY_SELECTOR, String(colorBy)),
         [URLSearchParam.dataset]: (id) => changeDataset(String(id)),
         [URLSearchParam.csvUrl]: (url) => setCsvUrl(decodeURIComponent(String(url))),
-        [URLSearchParam.galleryCollapsed]: (galleryCollapsed) => toggleGallery(galleryCollapsed === "true"),
+        [URLSearchParam.galleryCollapsed]: (galleryCollapsed) =>
+            toggleGallery(galleryCollapsed === "true"),
         [URLSearchParam.plotByOnX]: (plotByOnX) => changeAxis(X_AXIS_ID, String(plotByOnX)),
         [URLSearchParam.plotByOnY]: (plotByOnY) => changeAxis(Y_AXIS_ID, String(plotByOnY)),
         [URLSearchParam.selectedAlbum]: (album) => selectAlbum(Number(album)),
         [URLSearchParam.selectedPoint]: (selection) => {
             if (Array.isArray(selection)) {
-                return map<number | string, Action>(selection, (point) => selectPoint({ id: String(point)} ));
+                return map<number | string, Action>(selection, (point) =>
+                    selectPoint({ id: String(point) })
+                );
             }
-            return selectPoint({id: String(selection)});
+            return selectPoint({ id: String(selection) });
         },
     };
 
@@ -164,26 +191,30 @@ export default class UrlState {
         },
         [URLSearchParam.groupBy]: (groupBy) => ({ [GROUP_BY_KEY]: String(groupBy) }),
         [URLSearchParam.colorBy]: (colorBy) => ({ [COLOR_BY_SELECTOR]: String(colorBy) }),
-        [URLSearchParam.dataset]: (id) => ({dataset: String(id)}),
-        [URLSearchParam.galleryCollapsed]: (galleryCollapsed) => ({ galleryCollapsed: galleryCollapsed === "true" }),
+        [URLSearchParam.dataset]: (id) => ({ dataset: String(id) }),
+        [URLSearchParam.galleryCollapsed]: (galleryCollapsed) => ({
+            galleryCollapsed: galleryCollapsed === "true",
+        }),
         [URLSearchParam.plotByOnX]: (plotByOnX) => ({ [X_AXIS_ID]: String(plotByOnX) }),
         [URLSearchParam.plotByOnY]: (plotByOnY) => ({ [Y_AXIS_ID]: String(plotByOnY) }),
         [URLSearchParam.selectedAlbum]: (album) => ({ selectedAlbum: Number(album) }),
-        [URLSearchParam.selectedPoint]: (selection) => ({ initSelectedPoints: map(castArray(selection), String) }),
+        [URLSearchParam.selectedPoint]: (selection) => ({
+            initSelectedPoints: map(castArray(selection), String),
+        }),
     };
 
     private static stateToUrlParamMap: StateToUrlSearchParamMap = {
-        
         cellSelectedFor3D: (value) => ({ [URLSearchParam.cellSelectedFor3D]: String(value) }),
         [COLOR_BY_SELECTOR]: (value) => ({ [URLSearchParam.colorBy]: String(value) }),
         [GROUP_BY_KEY]: (value) => ({ [URLSearchParam.groupBy]: String(value) }),
-        dataset: (id) => ({ [URLSearchParam.dataset]: String(id)}),
+        dataset: (id) => ({ [URLSearchParam.dataset]: String(id) }),
         csvUrl: (url) => ({ [URLSearchParam.csvUrl]: String(encodeURIComponent(url)) }),
-        galleryCollapsed: (value) => ({ [URLSearchParam.galleryCollapsed]: String(value)}),
+        galleryCollapsed: (value) => ({ [URLSearchParam.galleryCollapsed]: String(value) }),
         selectedAlbum: (value) => ({ [URLSearchParam.selectedAlbum]: String(value) }),
         selectedPoints: (value: FileInfo[]) => {
             const arrayOfIds = map(value, (ele: FileInfo) => String(ele[CELL_ID_KEY]));
-            return { [URLSearchParam.selectedPoint]: arrayOfIds };},  
+            return { [URLSearchParam.selectedPoint]: arrayOfIds };
+        },
         [X_AXIS_ID]: (value) => ({ [URLSearchParam.plotByOnX]: String(value) }),
         [Y_AXIS_ID]: (value) => ({ [URLSearchParam.plotByOnY]: String(value) }),
     };
@@ -205,9 +236,6 @@ export default class UrlState {
     }
 
     private static valueIsNotDefault(selection: any, key: string): boolean {
-
-        return (initialState as any)[key] !== selection
-        
+        return (initialState as any)[key] !== selection;
     }
-
 }
